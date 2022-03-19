e763ecf00d6ec4a63eb963706d880056e816a1cf
Roy Schrauwen <roy@royschrauwen.nl>
2022-03-19 13:01:48 +0100

    New test

--- src/Components/CopyButton.js
+++ src/Components/CopyButton.js
@@ -3,7 +3,23 @@ import React from "react";
 function CopyButton({ translatedMessage }) {
   const handleCopyButtonClick = () => {
     navigator.clipboard.writeText(translatedMessage);
+    copyToClipboard(translatedMessage);
   };
+
+  function copyToClipboard(data) {
+    var tempItem = document.createElement("input");
+    tempItem.setAttribute("type", "text");
+    tempItem.setAttribute("display", "none");
+    let content = data;
+    if (data instanceof HTMLElement) {
+      content = data.innerHTML;
+    }
+    tempItem.setAttribute("value", content);
+    document.body.appendChild(tempItem);
+    tempItem.select();
+    document.execCommand("Copy");
+    tempItem.parentElement.removeChild(tempItem);
+  }
 
   return (
     <div>
