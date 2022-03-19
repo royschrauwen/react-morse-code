4c0cd56d78b74580d956da5595df320a111846e5
Roy Schrauwen <roy@royschrauwen.nl>
2022-03-19 11:45:25 +0100

    Copy Text to Clipboard

--- src/Components/CopyButton.js
+++ src/Components/CopyButton.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+export async function copyTextToClipboard(text) {
+  if ("clipboard" in navigator) {
+    return await navigator.clipboard.writeText(text);
+  } else {
+    return document.execCommand("copy", true, text);
+  }
+}
+
 function CopyButton({ translatedMessage }) {
   return (
     <div>
-      <button disabled className="btnCopy">
+      <button
+        onClick={copyTextToClipboard(translatedMessage)}
+        className="btnCopy"
+      >
         Copy
       </button>
     </div>
